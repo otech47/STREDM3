92d35241d6cbb4df57a836b6dc6c68bfcc506ead
gueroso <cjguerin93@gmail.com>
2014-03-15 02:55:38 -0400

    Reorganize Radio Mix

--- radiomixes.sql
+++ radiomixes.sql
@@ -42,28 +42,42 @@ CREATE TABLE IF NOT EXISTS `radiomixes` (
 --
 
 INSERT INTO `radiomixes` (`id`, `is_radiomix`, `radiomix`, `image_id`) VALUES
-(1, 1, 'A&B Group Therapy 48', 0),
-(2, 1, 'A&B Group Therapy 49', 0),
-(3, 1, 'A&B Group Therapy 62', 0),
-(4, 1, 'A&B Group Therapy 63', 0),
+(1, 1, 'Above & Beyond: Group Therapy', 0),
+(2, 1, 'Chuckie: Dirty Dutch Radio', 0),
+(3, 1, 'Fedde Le Grand: Dark Light Sessions', 0),
+(4, 1, 'Afrojack: Jacked', 0),
 (5, 1, 'BBC Radio 1 Essential Mix', 0),
 (6, 1, 'Dillon Francis - "GET LOW" Triple j Mix For Triple J Mix Ups', 0),
 (7, 1, 'Diplo & Friends', 0),
 (8, 1, 'Hardwell On Air', 0),
-(9, 1, 'Hardwell On Air 152', 0),
-(10, 1, 'Mainstage 176', 0),
-(11, 1, 'Mainstage 177', 0),
+(10, 1, 'Tiesto: Club Life', 0),
+(11, 1, 'W & W: Mainstage Mix', 0),
 (12, 1, 'Radio Mix: Group Therapy 050 Live from Alexandria Palace', 0),
 (13, 1, 'Radio Mix: Trance Around The World Guest Mix', 0),
 (14, 1, 'The HOT Sh*t', 0),
-(15, 1, 'The HOT Sh*t, Episode 97', 0),
-(16, 1, 'Troll Mix Vol. 9: Just The Tip *Valentine''s Day Edition*', 0),
+(15, 1, 'Global Mix', 0),
+(16, 1, 'Troll Mix', 0),
 (23, 1, 'Holiday Mix', 0),
-(22, 1, 'Above & Beyond Group Therapy ', 0),
+(22, 1, 'Simon Patterson: Open Up Mix', 0),
 (24, 1, 'Spring Mix', 0),
 (25, 1, 'Jambalaya Mix', 0),
 (26, 1, 'Mini Mix', 0),
-(27, 1, 'No Xcuses', 0);
+(28, 1, 'Laidback Luke: Mix Mash Radio', 0);
+(29, 1, 'EDX: No Xcuses', 0);
+(30, 1, 'Morgan Page: In The Air', 0);
+(31, 1, 'Myon & Shane 54: International Departure', 0);
+(32, 1, 'Gareth Emery: Podcast', 0);
+(33, 1, 'Paul Oakenfold: Planet Perfecto', 0);
+(34, 1, 'Aly & Fila: Future Sound of Egypt', 0);
+(35, 1, 'EDX: Dance Department', 0);
+(36, 1, 'Pete Tong: The Essential Selection', 0);
+(37, 1, 'Ferry Corsten: Corstens Countdown', 0);
+(38, 1, 'Ferry Corsten: Ferrys Fix', 0);
+(39, 1, 'Markus Schultz: Global DJ Broadcast', 0);
+(40, 1, 'Eric Prydz: EPIC Radio', 0);
+(41, 1, 'Carnage: Incredible', 0);
+(42, 1, 'Shadow Child: Rinse', 0);
+(43, 1, 'Sander Van Doorn: Identity', 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
